29b38bd77dc6ba49238919bc7507edc4377d0350
awaiskaleem <awaiskaleem@gmail.com>
2019-08-17 23:40:15 +0200

    test rows hard coded

--- packages/regression_model/tests/test_predict.py
+++ packages/regression_model/tests/test_predict.py
@@ -29,7 +29,7 @@ def test_make_multiple_predictions():
 
     # Then
     assert subject is not None
-    assert len(subject.get('predictions')) == 1451
+    assert len(subject.get('predictions')) == 418
 
     # We expect some rows to be filtered out
     assert len(subject.get('predictions')) != original_data_length
